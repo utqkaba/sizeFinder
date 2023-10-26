00c1f43df3e7d956598936b7289ff218edf24143
utqkaba <utqkaba@gmail.com>
2023-10-26 22:46:48 +0300

    promoBar animate

--- src/PromoBar.jsx
+++ src/PromoBar.jsx
@@ -21,7 +21,7 @@ function PromoBar() {
   return (
     <div className="bg-black text-white p-3 fixed top-0 w-screen">
       <div className="container text-center text-xs font-medium mx-auto">
-        <a href="https://uppstairs.com/">
+        <a href="https://uppstairs.com/" className='animate-pulse'>
           {promoText}
         </a>
       </div>
