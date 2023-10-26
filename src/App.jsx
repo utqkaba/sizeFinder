748350cb50500f870c6261f2e03702ac8877d8e8
utqkaba <utqkaba@gmail.com>
2023-10-26 15:10:37 +0300

    setSize fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import 'rc-slider/assets/index.css';
 export default function GfgInput() {
   const [height, setHeight] = useState(165)
   const [weight, setWeight] = useState(60)
-  // const [bmi, setBmi] = useState(0)
-  const [size, setSize] = useState("Large")
+  const [size, setSize] = useState("?")
 
   const handleHeightChange = (newValue) => {
     setHeight(newValue)
@@ -18,42 +17,26 @@ export default function GfgInput() {
 
   const handleBodyMassIndex = (newHeight, newWeight) => {
     const heightInMetre = newHeight / 100
-    const Bmi = newWeight / Math.pow(heightInMetre, 2)
-    switch (Bmi) {
-      case (Bmi < 18.5):
-        // setSize("Xs")
-        break;
-      case (18.5 < 24.9):
-        // setSize("S")
-        console.log(Bmi < 23);
+    const Bmi = (newWeight / Math.pow(heightInMetre, 2)).toFixed(2)
+    console.log(Bmi);
 
-        break;
-      case (25.0 < 29.9):
-        // setSize("M")
-        break;
-      case (30.0 < 34.9):
-        // setSize("L")
-        break;
-      case (35.0 < 39.9):
-        // setSize("Xl")
-        break;
-      case (Bmi > 40.0):
-        // setSize("Xxl")
-        break;
-      default:
-        break
+    if (Bmi < 18.5) {
+      setSize("XS")
+    } else if (Bmi >= 18.5 && Bmi < 24.9) {
+      setSize("M")
+    } else if (Bmi >= 25 && Bmi < 29.9) {
+      setSize("L")
+    } else if (Bmi >= 30 && Bmi < 34.9) {
+      setSize("L")
+    } else if (Bmi >= 35 && Bmi < 39.9) {
+      setSize("L")
+    } else {
+      setSize("XXXL")
     }
-    // setBmi(Bmi)
-    // isThisMySize(bmi)
   }
-  //fonskiyonları ayırabilirim. state güncellemede bir sorun çıktı, erteledim.
-  // const isThisMySize = (bmi) => {
-  //   console.log("lol:", bmi);
-
-  // }
 
   return (
-    <div className='flex flex-col items-center justify-center h-screen bg-white text-black'>
+    <div className='flex flex-col items-center justify-center h-screen bg-white text-black dark:bg-black dark:text-white'>
       <img src={"src/assets/vefaklnc.jpeg"} alt="Uppstairs" className='object-scale-down h-48 w-96' />
       <span className='font-extralight p-6 my-4'>Find out which size is the most suitable size right now!</span>
       <div className='w-[23%] border-2 border-double border-gray-800 rounded-xl'>
@@ -75,7 +58,7 @@ export default function GfgInput() {
       </div>
       <button className='border border-double border-gray-800 rounded-xl m-4 mt-6 px-4 font-thin animate-bounce hover:bg-gray-800 hover:text-white'
         onClick={() => { handleBodyMassIndex(height, weight) }}>Find Size</button>
-      <span className='font-medium text-lg'>Your size is '{size}' </span>
+      <span className='font-medium text-lg'>Your size is {size}. </span>
     </div>
   );
 }
