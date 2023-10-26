4b0ceb501d751edce16888edebb5b700e410c435
utqkaba <utqkaba@gmail.com>
2023-10-26 23:26:54 +0300

    width change

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ export default function GfgInput() {
         Find out which size is the most suitable size right now!
       </span>
 
-      <div className='w-[24%] border-2 border-double border-gray-800 rounded-xl'>
+      <div className='w-[20%] border-2 border-double border-gray-800 rounded-xl'>
         <div className='p-4 mx-6 text-center my-2'>
           <span className='font-thin text-base'>Height: {height} </span>
           <Slider
